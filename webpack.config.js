f1faaa26fcb6d7838e52c6fd67bb6251fb4e4f8f
theritesite <theritesites@gmail.com>
2021-01-10 11:41:50 -0700

    pushing updates from macbook

--- webpack.config.js
+++ webpack.config.js
@@ -40,6 +40,7 @@ const config = env => {
                     { from: path.resolve( __dirname, 'languages' ) + '/**', to: buildFolder },
                     { from: path.resolve( __dirname, 'public' ) + '/**', to: buildFolder },
                     { from: path.resolve( __dirname, 'README.*' ), to: buildFolder },
+                    { from: path.resolve( __dirname, 'woo-includes' ) + '/**', to: buildFolder },
                     { from: path.resolve( __dirname, 'LICENSE.txt' ), to: buildFolder },
                     { from: path.resolve( __dirname, 'CHANGELOG.*' ), to: buildFolder },
                     { from: path.resolve( __dirname, '*.php' ), to: buildFolder },
@@ -50,6 +51,7 @@ const config = env => {
                     { from: path.resolve( __dirname, 'languages' ) + '/**', to: endFolder },
                     { from: path.resolve( __dirname, 'public' ) + '/**', to: endFolder },
                     { from: path.resolve( __dirname, 'README.*' ), to: endFolder },
+                    { from: path.resolve( __dirname, 'woo-includes' ) + '/**', to: endFolder },
                     { from: path.resolve( __dirname, 'LICENSE.txt' ), to: endFolder },
                     { from: path.resolve( __dirname, 'CHANGELOG.*' ), to: endFolder },
                     { from: path.resolve( __dirname, '*.php' ), to: endFolder },
@@ -92,6 +94,7 @@ const config = env => {
                     { from: path.resolve( __dirname, 'languages' ) + '/**', to: devFolder },
                     { from: path.resolve( __dirname, 'public' ) + '/**', to: devFolder },
                     { from: path.resolve( __dirname, 'README.*' ), to: devFolder },
+                    { from: path.resolve( __dirname, 'woo-includes' ) + '/**', to: devFolder },
                     { from: path.resolve( __dirname, 'LICENSE.txt' ), to: devFolder },
                     { from: path.resolve( __dirname, 'CHANGELOG.*' ), to: devFolder },
                     { from: path.resolve( __dirname, '*.php' ), to: devFolder }
